1ba2ab2285ff5b8f0a5948ca87a387e7ff383f20
xiaolin <462441355@qq.com>
2020-07-11 18:30:26 +0800

    * 修改head

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,12 +13,14 @@ export default {
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
+  ** nuxt 兼容IE9及以上，flex 兼容IE10及以上，而国内浏览器大多采用webkit内核，或者双内核，双内核只需要简单的增加meta标签就可以了  <meta data-n-head="true" name="renderer" content="webkit">
   */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no' },
+      { name: 'renderer', content: 'webkit' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
