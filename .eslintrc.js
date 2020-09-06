076629343a50578117265c2a8b751d09f0d07ff1
xiaolin <462441355@qq.com>
2020-09-06 19:15:25 +0800

    * eslint 规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,18 +7,49 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
+  plugins: [],
   // add your custom rules here
   rules: {
     'no-console': 'off',
     'space-before-function-paren': 0,
     'indent': 'off',
     'eol-last': 'off',
-    'no-trailing-spaces': 'off'
+    'no-tabs': 'off',
+    'no-undef': 'off',
+    'no-trailing-spaces': 'off',
+    'unicorn/prefer-includes': 'off',
+    'padded-blocks': 'off',
+    'prefer-const': 'off',
+    'object-shorthand': 'off',
+    'curly': 'off',
+    'no-lonely-if': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'standard/no-callback-literal': 'off',
+    'quotes': 'off',
+    'no-unused-vars': 'off',
+    'import/first': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'import/no-mutable-exports': 'off',
+    'vue/multiline-html-element-content-newline': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/html-closing-bracket-spacing': 0,
+    'no-multiple-empty-lines': 0,
+    'arrow-parens': 'off',
+    'vue/no-v-html': 'off',
+    'semi': 'off',
+    'no-unused-vars': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'any',
+          component: 'any'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
   }
 }
